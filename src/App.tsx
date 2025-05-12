aa9603fbd2876d850b24efb5d76e92ee6fe07fb2
Neto test <nikolajeliseev834@gmail.com>
2025-05-12 20:59:36 +0300

    добавлена проверка на ввод неправельного hex-кода

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import useHexToRgb from './components/useHexToRgb';
 import './index.css';
 
 const App: FC = () => {
-  const { input, rgb, updateInput } = useHexToRgb();
+  const { input, rgb, isError, updateInput } = useHexToRgb();
 
   return (
-    <>
-    <div className="app" style={{ background: rgb }}>
-      <HexInput 
-        value={input} 
-        onChange={updateInput} 
-        rgb={rgb} 
+    <div
+      className='app'
+      style={{
+        background: isError ? 'rgb(143, 6, 6)' : input.length === 7 ? rgb : 'rgb(255, 255, 255)',
+      }}
+    >
+      <HexInput
+        value={input}
+        onChange={updateInput}
+        rgb={isError ? 'Ошибка!' : input.length === 7 ? rgb : 'rgb(255, 255, 255)'}
       />
     </div>
-    </>
   );
 };
 
--- src/components/useHexToRgb.tsx
+++ src/components/useHexToRgb.tsx
@@ -3,24 +3,34 @@ import { useState } from 'react';
 const useHexToRgb = () => {
   const [state, setState] = useState({
     input: '',
-    rgb: 'rgb(255, 255, 255)', 
+    rgb: 'rgb(255, 255, 255)',
+    isError: false,
   });
 
   const hexToRGB = (hex: string) => {
-    if (!hex) return 'rgb(255, 255, 255)';
-    if (!/^#[0-9A-Fa-f]{6}$/i.test(hex)) return 'rgb(255, 255, 255)';
+    if (!hex) return { rgb: 'rgb(255, 255, 255)', isError: false };
     
-    const r = parseInt(hex.slice(1, 3), 16);
-    const g = parseInt(hex.slice(3, 5), 16);
-    const b = parseInt(hex.slice(5, 7), 16);
+    if (hex.length === 7) {
+      if (!/^#[0-9A-Fa-f]{6}$/i.test(hex)) {
+        return { rgb: 'Ошибка!', isError: true };
+      }
+      
+      const r = parseInt(hex.slice(1, 3), 16);
+      const g = parseInt(hex.slice(3, 5), 16);
+      const b = parseInt(hex.slice(5, 7), 16);
+      
+      return { rgb: `rgb(${r}, ${g}, ${b})`, isError: false };
+    }
     
-    return `rgb(${r}, ${g}, ${b})`;
+    return { rgb: 'rgb(255, 255, 255)', isError: false };
   };
 
   const updateInput = (value: string) => {
+    const result = hexToRGB(value);
     setState({
       input: value,
-      rgb: hexToRGB(value),
+      rgb: result.rgb,
+      isError: result.isError,
     });
   };
 
